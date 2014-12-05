ad902ef29ce630e4b9520929cde39c526e0ce274
harrygfox <henry.george.fox@gmail.com>
2014-12-05 11:06:14 +0000

    Re-added that fucking js that I wrote yesterdat, piece of shit git

--- fac/js/founders.js
+++ fac/js/founders.js
@@ -1,3 +1,4 @@
+/* Adjust and animate navbar on scroll */
 $(window).scroll(function() {
     if ($(".navwrap").offset().top > 5) {
         $(".navwrap").addClass("navedit");
@@ -5,3 +6,18 @@ $(window).scroll(function() {
         $(".navwrap").removeClass("navedit");
     }
 });
+
+/* Set the second panel's divs to be the same size */
+$(document).ready(function() {
+   setHeight($('#titles-right-wrapper'), $('#tech-stack-wrapper'));
+    $(window).on(resize, function() {
+      setHeight($('#titles-right-wrapper'), $('#tech-stack-wrapper'));        
+    });
+        
+});
+
+// sets height of element 1 to equal the height of element 2
+function setHeight(elem1, elem2) {
+   var height = elem2.height()
+   elem1.css('height', height); 
+}
