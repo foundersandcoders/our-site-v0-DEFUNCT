6cd351fd5073e1297370446a5cfe97966ddcf205
harrygfox <henry.george.fox@gmail.com>
2014-12-05 12:18:09 +0000

    divs resize on load now at any window size

--- fac/js/founders.js
+++ fac/js/founders.js
@@ -4,17 +4,16 @@ $(window).scroll(function() {
         $(".navwrap").addClass("navedit");
     } else {
         $(".navwrap").removeClass("navedit");
-    }
+    };
 });
 
 /* Set the second panel's divs to be the same size */
-$(document).ready(function() {
+window.addEventListener('load', function() {
    setHeight($("#titles-right-wrapper"), $("#tech-stack-wrapper"));
     $(window).resize(function() {
       setHeight($("#titles-right-wrapper"), $("#tech-stack-wrapper"));        
-    });
-        
-});
+    }); 
+}, false);
 
 // sets height of element 1 to equal the height of element 2
 function setHeight(elem1, elem2) {
