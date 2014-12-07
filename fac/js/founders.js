c2283412b00aeecac0ae68056e436c65d0990754
harrygfox <henry.george.fox@gmail.com>
2014-12-07 20:01:11 +0000

    altered some styles and cleaned up the css and js, still sexy

--- fac/js/founders.js
+++ fac/js/founders.js
@@ -1,23 +1,35 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		Super sexy visual effects
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	Super sexy visual effects
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
-var techstack = document.getElementById("tech-stack");
-var spinImg = document.getElementById("spin-img");
-var navBar = document.getElementById("navbar");
-var title = document.getElementById("title");
-var speed = -2.5;
-var start = 150;
+/*--------------------------------------------------
+Variable Declarations */
+
 var mql = window.matchMedia("(min-width: 992px)");
+//Nav bar elements
+var start = 150;
+var nav = document.getElementById("nav");
+var spinWrapper = document.getElementById("spin-wrapper");
+var spinImg = document.getElementById("spin-img");
+var logo = document.getElementById("logo");
+var menu = document.getElementById("menu");
+// Landing page elements
+var title = document.getElementById("title");
+// Second panel elements
+var techStack = document.getElementById("tech-stack");
+var speed = -2.5;
 
-/* Set the parallax panel's divs to be the same size */
+
+/*--------------------------------------------------
+Set the parallax panel's divs to be the same size */
+
 window.addEventListener('load', function() {
-	
 	setHeight($("#titles-right"), $("#tech-stack-wrapper"));
 	$(window).resize(function() {
 		setHeight($("#titles-right"), $("#tech-stack-wrapper"));        
 	});
-
 }, false);
 
 // sets height of element 1 to equal the height of element 2
@@ -27,31 +39,38 @@ function setHeight(elem1, elem2) {
 };
 
 
-/* Parallax (operates only on larger screen sizes) */
+/*--------------------------------------------------
+Parallax and scroll movements 
+	(operate only on larger screen sizes) */
+
 if (mql.matches) {
 	window.onscroll = function() {		
 		var y = window.scrollY;
-		console.log(y);
-		techstack.style.transform = "translate(0px," + (y/speed) + "px)";
-		spinImg.style.transform = "rotate(" + (y/2) + "deg)";
+		// Parallax on techstack
+		techStack.style.transform = "translate(0px," + (y/speed) + "px)";
+		// Rotation and movement on nav
+		spinImg.style.transform = "rotate(" + (y/1.5) + "deg)";
 		title.style.transform = "translate(0px," + (y/speed) + "px)";
+		// Effects are dependent on start point
 		if (y >= start) {
-			navBar.style.height = (start - (y - start)) + "px";
-			navBar.classList.add("navedit");
-
-			if (navBar.offsetHeight < 51) {
-				navBar.style.height = "50px";
+			nav.style.height = (start - (y - start)) + "px";
+			nav.classList.add("navedit");
+			// and endpoint (which assumes success of lines above)
+			if (nav.offsetHeight < 51) {
+				nav.style.height = "50px";
 			}
 		}
 		else {
-			navbar.style.height = "150px";
-			navBar.classList.remove("navedit");
+			nav.style.height = "150px";
+			nav.classList.remove("navedit");
 		}
 	};
 } else {
 	console.log("parallax disabled");
 }
 
-	
+function moveUpFaster(elem) {
+	elem.style.transform = "translate(0px," + (y/speed) + "px)";
+}
 
 
