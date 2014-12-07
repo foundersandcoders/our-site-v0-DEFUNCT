8f1e93d223a4f1d88ce75866ee244bf017281b60
harrygfox <henry.george.fox@gmail.com>
2014-12-07 15:24:16 +0000

    altered behaviour of nav shrink to reflelect distance scrolled instead of time of transition

--- fac/js/founders.js
+++ fac/js/founders.js
@@ -1,49 +1,54 @@
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    Super sexy visual effects
+		Super sexy visual effects
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-/* Adjust and animate navbar on scroll */
-$(window).scroll(function() {
-    if ($(".navwrap").offset().top > 250) {
-        $(".navwrap").addClass("navedit");
-        //implementing nav spin
-        // document.getElementById("spin-img").style.transform = rotate( + ((window.scrollY)/3) deg);
-
-    } else {
-        $(".navwrap").removeClass("navedit");
-    };
-});
-
+var techstack = document.getElementById("tech-stack");
+var spinImg = document.getElementById("spin-img");
+var navBar = document.getElementById("navbar");
+var speed = -2.5;
+var start = 150;
+var mql = window.matchMedia("(min-width: 992px)");
 
 /* Set the parallax panel's divs to be the same size */
 window.addEventListener('load', function() {
-   setHeight($("#titles-right"), $("#tech-stack-wrapper"));
-    $(window).resize(function() {
-      setHeight($("#titles-right"), $("#tech-stack-wrapper"));        
-    }); 
+	
+	setHeight($("#titles-right"), $("#tech-stack-wrapper"));
+	$(window).resize(function() {
+		setHeight($("#titles-right"), $("#tech-stack-wrapper"));        
+	});
+
 }, false);
 
 // sets height of element 1 to equal the height of element 2
 function setHeight(elem1, elem2) {
-   var height = elem2.height()
-   elem1.css('height', height); 
+	var height = elem2.height()
+	elem1.css('height', height); 
 };
 
 
-/* Techstack parallax (operates only on larger screen sizes) */
-var techstack = document.getElementById("tech-stack");
-var spinImg = document.getElementById("spin-img");
-var speed = -2.5;
-var mql = window.matchMedia("(min-width: 992px)");
-
+/* Parallax (operates only on larger screen sizes) */
 if (mql.matches) {
-    window.onscroll = function() {
-        var y = window.scrollY;
-        techstack.style.transform = "translate(0px," + (y/speed) + "px)";
-        spinImg.style.transform = "rotate(" + (y/3) + "deg)";
-    };
+	window.onscroll = function() {
+		var y = window.scrollY;
+		techstack.style.transform = "translate(0px," + (y/speed) + "px)";
+		console.log(y);
+		spinImg.style.transform = "rotate(" + (y/2) + "deg)";
+		if (y >= start) {
+			navBar.style.height = (start - (y - start)) + "px";
+			navBar.classList.add("navedit");
+			if (navBar.offsetHeight < 51) {
+				navBar.style.height = "50px";
+			}
+		}
+		else {
+			navbar.style.height = "150px";
+			navBar.classList.remove("navedit");
+		}
+	};
 } else {
-    console.log("parallax disabled");
+	console.log("parallax disabled");
 }
+
+	
 
 
