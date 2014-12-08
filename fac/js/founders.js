b8a5560ce436c7c8cb6b94254cd8baba18b5d9d9
harrygfox <henry.george.fox@gmail.com>
2014-12-08 14:04:14 +0000

    slightly better timings on movements

--- fac/js/founders.js
+++ fac/js/founders.js
@@ -4,23 +4,6 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
-/*--------------------------------------------------
-Variable Declarations */
-
-var mql = window.matchMedia("(min-width: 992px)");
-//Nav bar elements
-var start = 150;
-var nav = document.getElementById("nav");
-var spinWrapper = document.getElementById("spin-wrapper");
-var spinImg = document.getElementById("spin-img");
-var logo = document.getElementById("logo");
-var menu = document.getElementById("menu");
-// Landing page elements
-var title = document.getElementById("title");
-// Second panel elements
-var techStack = document.getElementById("tech-stack");
-var speed = -2.5;
-
 
 /*--------------------------------------------------
 Set the parallax panel's divs to be the same size */
@@ -40,37 +23,62 @@ function setHeight(elem1, elem2) {
 
 
 /*--------------------------------------------------
+Variable Declarations */
+
+var mql = window.matchMedia("(min-width: 992px)");
+//Nav bar elements
+var start = 150;
+var end = 50;
+var nav = document.getElementById("nav");
+var spinWrapper = document.getElementById("spin-wrapper");
+var spinImg = document.getElementById("spin-img");
+var logo = document.getElementById("logo");
+var menu = document.getElementById("menu");
+// Landing page elements
+var title = document.getElementById("title");
+// Second panel elements
+var techStack = document.getElementById("tech-stack");
+var speed = -2.5;
+
+
+/*--------------------------------------------------
 Parallax and scroll movements 
 	(operate only on larger screen sizes) */
 
 if (mql.matches) {
+	var y = window.scrollY;
+	if (y >= start + (start - end)) {
+        nav.style.height = "50px";
+        nav.classList.add("navedit");
+    }
 	window.onscroll = function() {		
 		var y = window.scrollY;
+		// Rotation on nav img
+		spinImg.style.transform = "rotate(" + (y/1.5) + "deg)";
+		// Movement up of title
+		title.style.transform = "translate(0px," + (y/speed) + "px)";
 		// Parallax on techstack
 		techStack.style.transform = "translate(0px," + (y/speed) + "px)";
-		// Rotation and movement on nav
-		spinImg.style.transform = "rotate(" + (y/1.5) + "deg)";
-		title.style.transform = "translate(0px," + (y/speed) + "px)";
-		// Effects are dependent on start point
+		// Nav element Effects are dependent on start point
 		if (y >= start) {
 			nav.style.height = (start - (y - start)) + "px";
-			nav.classList.add("navedit");
 			// and endpoint (which assumes success of lines above)
-			if (nav.offsetHeight < 51) {
+			if (nav.offsetHeight <= end) {
 				nav.style.height = "50px";
 			}
+			// delay applying css transition
+			if (y >= (start + end)) {
+				nav.classList.add("navedit");
+			} else {
+				nav.classList.remove("navedit");
+			}
 		}
-		else {
-			nav.style.height = "150px";
-			nav.classList.remove("navedit");
-		}
+		// if (y >= start + (start - end)) {
+  //           nav.style.height = "50px";
+  //       }
 	};
 } else {
-	console.log("parallax disabled");
-}
-
-function moveUpFaster(elem) {
-	elem.style.transform = "translate(0px," + (y/speed) + "px)";
+	console.log("parallax disabled below 992px");
 }
 
 
