3e67ad5f55ecb4160db6ec8c52031bda23d64fa2
Izaak Sofer <izaaksofer@googlemail.com>
2014-12-09 13:04:30 +0000

    curriculum

--- fac/js/angular.js
+++ fac/js/angular.js
@@ -32,10 +32,10 @@
               url: "team",
               templateUrl: "../assets/ui-router-includes/team.html",
           })          
-          .state('faq', {
-              url: "faq",
-              templateUrl: "../assets/ui-router-includes/faq.html"
-          })
+        .state('faq', {
+            url: "faq",
+            templateUrl: "../assets/ui-router-includes/faq.html"
+        })
           .state('faq.general', {
                 url: "general",
                 templateUrl: "../assets/ui-router-includes/general.html",
@@ -51,6 +51,50 @@
           .state('faq.applicants', {
                 url: "applicants",
                 templateUrl: "../assets/ui-router-includes/applicants.html",
+            })
+        .state('curriculum', {
+          url: "curriculum",
+          templateUrl: "../assets/ui-router-includes/curriculum.html"
+        })
+          .state('curriculum.precourse', {
+                url: "precourseeee",
+                templateUrl: "../assets/ui-router-includes/curriculum/precourse.html",
+            })
+          .state('curriculum.week1', {
+                url: "week1",
+                templateUrl: "../assets/ui-router-includes/curriculum/week1.html",
+            })
+          .state('curriculum.week2', {
+                url: "week2",
+                templateUrl: "../assets/ui-router-includes/curriculum/week2.html",
+            })
+          .state('curriculum.week3', {
+                url: "week3",
+                templateUrl: "../assets/ui-router-includes/curriculum/week3.html",
+            })
+          .state('curriculum.week4', {
+                url: "week4",
+                templateUrl: "../assets/ui-router-includes/curriculum/week4.html",
+            })
+          .state('curriculum.week5', {
+                url: "week5",
+                templateUrl: "../assets/ui-router-includes/curriculum/week5.html",
+            })
+          .state('curriculum.week6', {
+                url: "week6",
+                templateUrl: "../assets/ui-router-includes/curriculum/week6.html",
+            })
+          .state('curriculum.week7', {
+                url: "week7",
+                templateUrl: "../assets/ui-router-includes/curriculum/week7.html",
+            })
+          .state('curriculum.week8', {
+                url: "week8",
+                templateUrl: "../assets/ui-router-includes/curriculum/week8.html",
+            })
+          .state('curriculum.week9-16', {
+                url: "week9-16",
+                templateUrl: "../assets/ui-router-includes/curriculum/week9-16.html",
             })
 
           
